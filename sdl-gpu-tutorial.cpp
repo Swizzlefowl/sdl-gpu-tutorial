0a8b60008dfde0c1f45d66fdf20a63885d3941cc
Arbaz <arbazk1444@gmail.com>
2025-11-01 15:55:52 +0530

    Add project files.

---
+++ sdl-gpu-tutorial.cpp
@@ -0,0 +1,12 @@
+﻿// sdl-gpu-tutorial.cpp : Defines the entry point for the application.
+//
+
+#include "sdl-gpu-tutorial.h"
+#include <fmt/core.h>
+using namespace std;
+
+int main()
+{
+	fmt::println("hello");
+	return 0;
+}
